ded1dffa70602706da89fb46a7997cb4d05bfac1
KontraSystem <samuraj.tiger430@passinbox.com>
2026-02-08 14:04:11 +0100

    add enums, fix import urls

---
+++ scripts/enums/bodyPosition.enum.gd
@@ -0,0 +1,6 @@
+@export enum BodyPositionEnum {
+    BALANCED = "balanced" # upright position
+    OVERHANGING = "overhanging" # leaning away from wall
+    COMPRESSION = "compression" # pushing against opposite surfaces
+    DYNO = "dyno" # Dynamic movement
+}
---
+++ scripts/enums/legPosition.enum.gd
@@ -0,0 +1,7 @@
+@export enum LegPositionEnum {
+    STANDING = "standing" # neutral stance, no stamina loss
+    HANGING = "hanging" # arm stress, stamina loss
+    FLAGGING = "flagging" # leg extended, minimal stamina loss
+    STEMMING = "stemming" # both legs pressure on opposite sides, moderate stamina loss
+    HEEL_HOOK = "hook" # hooking heel for stability, minimal stamina loss
+}
